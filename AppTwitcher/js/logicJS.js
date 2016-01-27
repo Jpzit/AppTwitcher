62333c45c20a6800ce7db3961a51474581a16d34
Maxime <maxnard35@gmail.com>
2016-01-27 11:27:40 -0500

    ckeck if stream is live works

--- AppTwitcher/js/logicJS.js
+++ AppTwitcher/js/logicJS.js
@@ -1,5 +1,24 @@
 function alertNotLive(){
   alert("Stream is not currently live.");
 }
+function alertLive(){
+	alert("Stream is live.");
+}
+function checkIfLive(){
+    var URL = document.getElementById('URLTextBox').value;
+	$.getJSON("https://api.twitch.tv/kraken/streams/" + URL , function(channel)
+		{
+			if (channel.stream == null) { 
+				alertNotLive();
+			}
+			else{
+				alertLive();
+			}
+		});
+}
+
 var confirmBtn = document.getElementById('confirmBtn');
-confirmBtn.addEventListener('click', alertNotLive);
+confirmBtn.addEventListener('click', checkIfLive);
+
+
+
